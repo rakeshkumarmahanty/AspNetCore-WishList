31b9093a597eaca0b210536834ed40aaf3e4276a
Rakesh Mahanty <rakeshkumarmahanty@gmail.com>
2020-11-01 21:24:57 -0600

    Update Item controller
    Update Item controller

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ItemController(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View("Create");
+        }
+        [HttpPost]
+        public IActionResult Create(WishList.Models.Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return View("Index");
+        }
+        public IActionResult Delete(int Id)
+        {
+            _context.SaveChanges();
+            return View("Index");
+        }
+    }
+}
+
