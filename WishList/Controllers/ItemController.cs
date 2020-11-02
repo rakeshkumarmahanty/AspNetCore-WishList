1cd8b51d7733753f2cc87972f49954c8c55fb8f9
Rakesh Mahanty <rakeshkumarmahanty@gmail.com>
2020-11-01 21:34:26 -0600

    Update ItemController.cs
    Update

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -26,7 +26,7 @@ namespace WishList.Controllers
             return View("Create");
         }
         [HttpPost]
-        public IActionResult Create(WishList.Models.Item item)
+        public IActionResult Create(Models.Item item)
         {
             _context.Items.Add(item);
             _context.SaveChanges();
